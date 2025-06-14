e136cf36193aa2937eed4c09084837d69820d7c1
fatihaltiok <dundeefatih@gmail.com>
2025-06-14 16:41:35 +0200

    feat: Füge calculator.py zur Testprojektstruktur hinzu

---
+++ test_project/calculator.py
@@ -0,0 +1,8 @@
+# test_project/calculator.py
+def add(a, b):
+    """This function adds two numbers."""
+    return a + b
+
+def subtract(a, b):
+    """This function subtracts two numbers."""
+    return a - b
