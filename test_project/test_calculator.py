5acb7fbdbedc28116fd3776d3a0a049ddf304a0e
fatihaltiok <dundeefatih@gmail.com>
2025-06-23 00:21:37 +0200

    refactor: importiere ConfigManager aus dem richtigen Modul

--- test_project/test_calculator.py
+++ test_project/test_calculator.py
@@ -1,12 +1,8 @@
-import pytest
-from test_project.calculator import add
-from test_project.utils import ConfigManager
 
-def test_add():
-    assert add(2, 3) == 5
-    assert add(-1, 1) == 0
-    assert add(0, 0) == 0
+import pytest
+from config_manager import ConfigManager
+
+
 def test_config_manager():
     config_manager = ConfigManager('test_project/config.json')
-    config_manager.load_config()
     assert config_manager.get_value('version') == '1.0'
